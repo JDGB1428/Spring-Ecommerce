889df8da6d189882614bd7707ab91da8c0a98e2f
JuanDavid <juangalindo28@hotmail.com>
2022-01-04 16:35:27 -0500

    añadiendo repository

---
+++ src/main/java/com/juan/marketplaces/repository/DetalleOrdenRepository.java
@@ -0,0 +1,9 @@
+package com.juan.marketplaces.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.juan.marketplaces.models.DetalleOrden;
+
+public interface DetalleOrdenRepository extends JpaRepository<DetalleOrden, Long>{
+
+}
